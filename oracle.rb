2bd7063d9cbbd51123dca3acbaa9bddd122fb11c
Vterebenin <niceserver11@mail.ru>
2018-04-10 19:23:09 +0300

    take action in Modules chapter

---
+++ oracle.rb
@@ -0,0 +1,8 @@
+
+require 'net/http'
+
+h = Net::HTTP.new('www.pragmaticprogrammer.com', 80)
+resp, data = h.get('/index.html', nil)
+if resp.message == "OK"
+  data.scan(/<img src="(.*?)"/) { |x| puts x }
+end
