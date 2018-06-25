7caf2f97e22a9bf66a886028f0d83a4b6376c8ad
Vterebenin <niceserver11@mail.ru>
2018-06-26 00:40:24 +0300

    27q.rb

--- codewars/27q.rb
+++ codewars/27q.rb
@@ -1,22 +1,27 @@
-def buddy(start, nd)
-  answer = ""
-  (start..nd).each_with_index do |e, i|
-  	p "m " + (m = e).to_s
-  	p summ_of_m = (1..Math.sqrt(m)).select { |n| m % n == 0}.sum
-
-  	if !(summ_of_m == 0)
-  		p summ_of_m = (m / summ_of_m)
-  		p "n " + (n = summ_of_m - 1).to_s
-  		p summ_of_n = (1..Math.sqrt(n)).select { |m| n % m == 0}.sum
-  		summ_of_n = (n / summ_of_n) if !(summ_of_n == 0)
-  		if (summ_of_m == n + 1) && (summ_of_n == m + 1)
-  			return p answer = "(#{m} #{n})"
-  			break
-  		end
-  	end
+def div_sum(n)
+  sum_of_n = 1
+  (2..Math.sqrt(n)).each do |i|
+    sum_of_n += n/i + i if n % i == 0
   end
-  p "Nothing" if answer.nil?
+  sum_of_n
 end
+
+def buddy(start, nd)
+  (start..nd).each do |e|
+    m = e
+    sum_of_m = div_sum(m)
+    if !(sum_of_m == 0)
+      "n " + (n = sum_of_m - 1).to_s
+      sum_of_n = div_sum(n)
+      if (sum_of_m == n + 1) && (sum_of_n == m + 1)
+        return "(#{m} #{n})" if  m < n
+      end
+    end
+  end
+  "Nothing"
+end
+
+
 p buddy(48, 50)
-# p buddy(1071625, 1103735)
+p buddy(1071625, 1103735)
  #p Math.sqrt(1071642).round
